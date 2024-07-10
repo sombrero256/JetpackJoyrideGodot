0c9e485eb9845de4066b6c065f321f61992eef2c
MRohrssenCoe <70444334+MRohrssenCoe@users.noreply.github.com>
2024-07-10 18:15:24 -0500

    infinite running enabled

--- Compositions/GameManager.gd
+++ Compositions/GameManager.gd
@@ -5,11 +5,37 @@ var Coins_Collected = 0
 var Difficulty = 1
 
 @onready var Player := $PlayerCharacter as Player
+@onready var RoomScene = preload("res://Compositions/room.tscn")
+@onready var RootRoom := $Room as Room
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# spawn second starting room
+	add_room_at_end()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	Handle_Environment(delta)
 	pass
+
+
+func Handle_Environment(delta):
+	RootRoom.move_room_list(delta)
+	if RootRoom.RoomSnap.position.x <= 0:
+		add_room_at_end()
+		var t = RootRoom
+		RootRoom = RootRoom.NextRoom
+		RootRoom.PrevRoom = null
+		t.queue_free()
+	pass
+	
+func add_room_at_end():
+	var n = RootRoom
+	while n.NextRoom != null:
+		n = n.NextRoom
+	var TempRoom = RoomScene.instantiate() as Room
+	TempRoom.PrevRoom = n
+	n.NextRoom = TempRoom
+	TempRoom.position = n.RoomSnap.position
+	add_child(TempRoom)
--- Compositions/room.gd
+++ Compositions/room.gd
@@ -14,18 +14,10 @@ func _init(PR = null, NR = null, S = Speed):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# if both PrevRoom and NextRoom are null this is the first node
-	# instantiate next node and place at end
-	if PrevRoom == null and NextRoom == null:
-		# spawn next room in 
-		NextRoom = Room.new(self, null, Speed)
-		add_child(NextRoom)
-		pass
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	move_room_list(delta)
 	pass
 
 func move_room_list(delta):
@@ -34,6 +26,7 @@ func move_room_list(delta):
 		Speed = PrevRoom.Speed
 	else:
 		translate(Vector2(-Speed * delta, 0))
+		RoomSnap.translate(Vector2(-Speed * delta, 0))
 	if NextRoom != null:
 		NextRoom.move_room_list(delta)
 		pass
